9e4ba400cfcb7bc0901b78a246ec59bcb4536f74
luniana217 <hehe627@gmail.com>
2026-04-15 11:49:14 +0900

    수정후

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,3 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { BrowserRouter, Routes, Route } from "react-router";
 
 const Home = () => {
